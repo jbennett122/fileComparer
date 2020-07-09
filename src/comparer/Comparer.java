df777a64a3fc33e63e1ed48eed242353e270a63c
jbennett122 <jjb2474@gmail.com>
2020-07-09 11:45:23 -0500

    file progression

--- src/comparer/Comparer.java
+++ src/comparer/Comparer.java
@@ -17,6 +17,13 @@ import java.*;
 public class Comparer {
 
 	
+	class person{
+		
+		public String last;
+		public String first;
+		
+	};
+	
 	
 	/**
 	 * 
@@ -30,18 +37,23 @@ public class Comparer {
 	 */
 	public static void main(String[] args) {
 		// TODO compare two csv/excel name lists 
-		
+		String string;
 		
 		//array for AD holding person data types
-		ArrayList AD = new ArrayList<Object>();
-		
+		ArrayList AD = new ArrayList<person>();
 		
 		//array for atcts holding person data types
-		ArrayList ATCTS = new ArrayList<Object>();
+		ArrayList ATCTS = new ArrayList<person>();
 		
 		//hardcoded csv file loaded into AD array
 		//TODO add file location 
-		FileReader ADFILE = new FileReader("20200709180724_9188.csv");
+		FileReader ADFILE;
+		try {
+			ADFILE = new FileReader("20200709180724_9188.csv");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.getLocalizedMessage("file not found";
+		}
 		BufferedReader ADF=new BufferedReader(ADFILE);
 		
 		
@@ -49,19 +61,19 @@ public class Comparer {
 		FileReader ATFILE = new FileReader("20200709180724_9188.csv");
 		
 		//while not eof load last, first into person object
-		while(!){
+		
 			
-			ADF.readLine().split(regex)
+			
 			//grab both last name first name using loop
 			
-			AD.add(ATFILE.)		
+			AD.add(ADF.readLine().split("\\r?\\n"));		
 			
 				//person.last = ADFILE
 				
 			
 			
-		}
 		
+	
 		
 			
 
