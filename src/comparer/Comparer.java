1beeeb9b2d00903b1c961aadd59fa550cfcdebcf
jbennett122 <jjb2474@gmail.com>
2020-07-09 11:22:22 -0500

    Setting up framework of process

---
+++ src/comparer/Comparer.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package comparer;
+
+import java.util.ArrayList;
+/**
+ * @author Robin
+ *
+ */
+import java.lang.*;
+import java.io.*;
+import java.*;
+
+
+
+public class Comparer {
+
+	
+	
+	/**
+	 * 
+	 */
+	public Comparer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO compare two csv/excel name lists 
+		
+		
+		//array for AD holding person data types
+		ArrayList AD = new ArrayList<Object>();
+		
+		
+		//array for atcts holding person data types
+		ArrayList ATCTS = new ArrayList<Object>();
+		
+		//hardcoded csv file loaded into AD array
+		//TODO add file location 
+		FileReader ADFILE = new FileReader("20200709180724_9188.csv");
+		BufferedReader ADF=new BufferedReader(ADFILE);
+		
+		
+		//hardcoded csv file loaded into ATCTS array
+		FileReader ATFILE = new FileReader("20200709180724_9188.csv");
+		
+		//while not eof load last, first into person object
+		while(!){
+			
+			ADF.readLine().split(regex)
+			//grab both last name first name using loop
+			
+			AD.add(ATFILE.)		
+			
+				//person.last = ADFILE
+				
+			
+			
+		}
+		
+		
+			
+
+	}
+	private static void inputfiles(){}
+}
