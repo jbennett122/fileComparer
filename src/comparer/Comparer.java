e55a9fc6551b36635c4b363340096a7d8fb494b9
Justin <justinbennett122@yahoo.com>
2020-07-10 14:13:20 -0500

    I think it works!

--- src/comparer/Comparer.java
+++ src/comparer/Comparer.java
@@ -4,6 +4,12 @@
 package comparer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.Set;
 /**
  * @author Robin
  *
@@ -11,6 +17,7 @@ import java.util.ArrayList;
 import java.lang.*;
 import java.io.*;
 import java.*;
+ 
 
 
 
@@ -38,51 +45,87 @@ public class Comparer {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO compare two csv/excel name lists 
-		String string;
+		 List<String> s1;
+		 List<String> s2;
+		 int counter=0;
+		 
+		 
+		 ListIterator<List<String>> ADlitr = null;
+		 ListIterator<List<String>> ATlitr2 = null;
+		 
+	//	System.out.println("test test1");
+			
 		
-		//array for AD holding person data types
-		ArrayList AD = new ArrayList<person>();
-		
-		//array for atcts holding person data types
-		ArrayList ATCTS = new ArrayList<person>();
-		
-		//hardcoded csv file loaded into AD array
-		//TODO add file location 
-		FileReader ADFILE = null;
-		try {
-			ADFILE = new FileReader("20200709180724_9188.csv");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.getLocalizedMessage();
-		}
-		BufferedReader ADF=new BufferedReader(ADFILE);
-		
-		
-		//hardcoded csv file loaded into ATCTS array
-		try {
-			FileReader ATFILE = new FileReader("20200709180724_9188.csv");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		ArrayList<List<String>> AD = new ArrayList<>();
+		//20200709180724_9188
+		//book4
+	//	try (Scanner scanner = new Scanner(new File("20200709180724_9188.csv"));) {
+		try (Scanner scanner = new Scanner(new File("AD.csv"));) {
+		    while (scanner.hasNextLine()) {
+		    	AD.add(getRecordFromLine(scanner.nextLine()));
+		    }
 		}
 		
-		//while not eof load last, first into person object
-		
-			
-			
-			//grab both last name first name using loop
-			
-			AD.add(ADF.readLine().split("\\r?\\n"));		
-			
-				//person.last = ADFILE
-				
-			
-			
-		
+		ArrayList<List<String>> ATCTS = new ArrayList<>();
+		//20200709180724_9188b
+		//book3
+	//	try (Scanner scanner = new Scanner(new File("20200709180724_9188b.csv"));) {
+		try (Scanner scanner = new Scanner(new File("AT.csv"));) {
+		    while (scanner.hasNextLine()) {
+		        ATCTS.add(getRecordFromLine(scanner.nextLine()));
+		    }
+		}
+							
+	System.out.println(AD.size());
 	
 		
+		//iterator thru Active directory list looking for matches in ATCTS list
+			//if not found export to new list
+		ADlitr=AD.listIterator();
+		
+		
+		while(ADlitr.hasNext()) {
 			
-
+			s1=ADlitr.next();
+						
+					
+	//System.out.println(s1+ " - AD");
+			ATlitr2=ATCTS.listIterator();
+			while(ATlitr2.hasNext()) {
+				
+				//System.out.println(litr2.next());
+				s2=ATlitr2.next();
+								
+			//	System.out.println(s2 + " - AT");
+					
+				if(s1.equals(s2)) {
+					counter++;
+					
+				System.out.println(s1 + " is " +  s2);
+			
+				break;
+					}
+			 
+			
+				}
+	
+		}
+			
+		
+		System.out.println(counter + " matches");	
+		
+	 
+			
 	}
-	private static void inputfiles(){}
+	private static List<String> getRecordFromLine(String line) {
+	    List<String> values = new ArrayList<String>();
+	    try (Scanner rowScanner = new Scanner(line)) {
+	        rowScanner.useDelimiter("\\r?\\n");
+	        while (rowScanner.hasNext()) {
+	            values.add(rowScanner.next());
+	        }
+	    }
+	    return values;
+	}
+	
 }
